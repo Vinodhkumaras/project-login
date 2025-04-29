f445487d99e1c6a2720bc0358f813a0c953fcd01
vinodhkumaras <vinodhvk24@gmail.com>
2025-04-29 12:28:38 +0530

    first commit

---
+++ tools.py
@@ -0,0 +1,17 @@
+import os
+import time
+
+def clear():
+     # this will pass the 'cls' command to the power shell session runing the program
+    os.system('cls')
+
+def sleep(seconds=2):
+    # this makes the interpreter 'sleep' for given seconds
+    time.sleep(seconds)
+
+
+def quit():
+    clear()
+    print("bye byee...")
+    sleep(3)
+    os.system('exit')
